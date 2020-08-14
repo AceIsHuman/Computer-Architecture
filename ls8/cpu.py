ae86e6b991a3ed5d2a88ebbfb5913ea79f23d1f2
AceIsHuman <menoman1995@yahoo.com>
2020-08-13 23:17:22 -0500

    refactor code

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,3 +1,13 @@
+HLT  = 0b00000001
+LDI  = 0b10000010
+PRN  = 0b01000111
+ADD  = 0b10100000
+MUL  = 0b10100010
+PUSH = 0b01000101
+POP  = 0b01000110
+CALL = 0b01010000
+RET  = 0b00010001
+
 """CPU functionality."""
 
 import sys
@@ -15,6 +25,17 @@ class CPU:
         self.reg[7] = 0xF4
         self.pc = 0
         self.running = False
+
+        self.instructions = {}
+        self.instructions[HLT] = self.hlt
+        self.instructions[LDI] = self.ldi
+        self.instructions[PRN] = self.prn
+        self.instructions[ADD] = self.add
+        self.instructions[MUL] = self.mul
+        self.instructions[PUSH] =self.push
+        self.instructions[POP] = self.pop
+        self.instructions[CALL] =self.call
+        self.instructions[RET] = self.ret
 
     def load(self, path):
         """Load a program into memory."""
@@ -73,81 +94,6 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        HLT  = 0b00000001
-        LDI  = 0b10000010
-        PRN  = 0b01000111
-        ADD  = 0b10100000
-        MUL  = 0b10100010
-        PUSH = 0b01000101
-        POP  = 0b01000110
-        CALL = 0b01010000
-        RET  = 0b00010001
-
-        def hlt():
-            print('Shutting Down... Goodbye')
-            self.running = False
-            sys.exit(0)
-
-        def ldi():
-            reg = self.ram_read(self.pc + 1)
-            val = self.ram_read(self.pc + 2)
-            self.reg[reg] = val
-
-        def prn():
-            reg = self.ram_read(self.pc + 1)
-            print(self.reg[reg])
-
-        def add():
-            operand_a, operand_b = self.ram_read(self.pc + 1), self.ram_read(self.pc + 2)
-            self.alu("ADD", operand_a, operand_b)
-
-        def mul():
-            operand_a, operand_b = self.ram_read(self.pc + 1), self.ram_read(self.pc + 2)
-            self.alu("MUL", operand_a, operand_b)
-
-        def call():
-            # get address for instruction
-            instruction_address = self.ram_read(self.pc + 1)
-            # get the next instruction address
-            next_address = self.pc + 2
-            ## store in stack
-            self.reg[7] -= 1
-            self.ram_write(next_address, self.reg[7])
-            self.pc = self.reg[instruction_address]
-
-        def ret():
-            ret_address = self.ram_read(self.reg[7])
-            self.pc = ret_address
-            self.reg[7] += 1
-
-        def push():
-            # decrement stack pointer
-            self.reg[7] -= 1
-            # read register value
-            reg = self.ram_read(self.pc + 1)
-            val = self.reg[reg]
-            # write to ram at stack pointer
-            self.ram_write(val, self.reg[7])        
-
-        def pop():
-            # get value at current stack pointer address
-            val = self.ram_read(self.reg[7])
-            # store value at given register
-            reg = self.ram_read(self.pc + 1)
-            self.reg[reg] = val
-            # increment stack pointer
-            self.reg[7] += 1
-  
-        instructions = {}
-        instructions[HLT] = hlt
-        instructions[LDI] = ldi
-        instructions[PRN] = prn
-        instructions[ADD] = add
-        instructions[MUL] = mul
-        instructions[PUSH] = push
-        instructions[POP] = pop
-        instructions[CALL] = call
-        instructions[RET] = ret
 
         self.running = True
         # read memory address stored in PC
@@ -156,14 +102,67 @@ class CPU:
         while self.running:
             ir = self.ram_read(self.pc)
             num_operands = ir >> 6
-            instructions[ir]()
+            op_a, op_b = self.ram_read(self.pc + 1), self.ram_read(self.pc + 2)
+
+            self.instructions[ir](op_a, op_b)
+
             prevent_pc_update = (ir >> 4) & 1
             if prevent_pc_update:
                 continue
-            self.pc += num_operands + 1
+            self.pc += num_operands + 1 
 
     def ram_read(self, mar):
         return self.ram[mar]
 
     def ram_write(self, mdr, mar):
         self.ram[mar] = mdr
+
+
+    def hlt(self, *_):
+        print('Shutting Down... Goodbye')
+        self.running = False
+        sys.exit(0)
+
+    def ldi(self, reg, val):
+        self.reg[reg] = val
+
+    def prn(self, reg, _):
+        print(self.reg[reg])
+
+    def add(self, op_a, op_b):
+        self.alu("ADD", op_a, op_b)
+
+    def mul(self, op_a, op_b):
+        self.alu("MUL", op_a, op_b)
+
+    def call(self, inst_address, _):
+        # get the next instruction address
+        next_address = self.pc + 2
+        ## store in stack
+        self.reg[7] -= 1
+        self.ram_write(next_address, self.reg[7])
+
+        # go to instruction
+        self.pc = self.reg[inst_address]
+
+    def ret(self, *_):
+        ret_address = self.ram_read(self.reg[7])
+        self.pc = ret_address
+        self.reg[7] += 1
+
+    def push(self, reg, _):
+        # decrement stack pointer
+        self.reg[7] -= 1
+        # read register value
+        val = self.reg[reg]
+        # write to ram at stack pointer
+        self.ram_write(val, self.reg[7])        
+
+    def pop(self, reg, _):
+        # get value at current stack pointer address
+        val = self.ram_read(self.reg[7])
+        # store value at given register
+        self.reg[reg] = val
+        # increment stack pointer
+        self.reg[7] += 1
+
